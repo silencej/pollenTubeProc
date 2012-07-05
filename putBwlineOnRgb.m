b89c02d9262294bbba0d68ed6eb429fc97d3d8ff
Chaofeng Wang <owen263@gmail.com>
2012-07-05 23:17:21 +0800

    small modi

--- putBwlineOnRgb.m
+++ putBwlineOnRgb.m
@@ -1,15 +1,19 @@
-function imgOut=putBwlineOnRgb(img,bw)
+function imgOut=putBwlineOnRgb(img,bw,color)
 % imgOut=putBwlineOnRgb(img,bw)
 % bw should be a linear bw image.
 
+if nargin<3
+    color=[255 255 255];
+end
+
 tempLayer=img(:,:,1); % Layer for temp use.
-tempLayer(bw)=255;
+tempLayer(bw)=color(1);
 imgOut(:,:,1)=tempLayer;
 tempLayer=img(:,:,2); % oriPart 1 layer for temp use.
-tempLayer(bw)=255;
+tempLayer(bw)=color(2);
 imgOut(:,:,2)=tempLayer;
 tempLayer=img(:,:,3); % oriPart 1 layer for temp use.
-tempLayer(bw)=255;
+tempLayer(bw)=color(3);
 imgOut(:,:,3)=tempLayer;
 
 end
--- testSkel.m
+++ testSkel.m
@@ -4,8 +4,9 @@ function testSkel
 %%
 close all;
 
-img=imread('../data/129-12.bw.png');
-grayOri=getGrayImg(img);
+bw=imread('../data/test/129-12.bw.png');
+% bw=imread('../data/test/2575 DIC115.bw.png');
+% grayOri=getGrayImg(img);
 
 tic;
 thinRes=bwmorph(bw,'thin',inf);
@@ -19,10 +20,23 @@ t1=toc;
 % skel=div_skeleton_new(4,1,1-bw,0);
 % [skel,I0,x,y,x1,y1,aa,bb]=div_skeleton_new(4,1,1-bw,60);
 
-figure,imshow();
+rgbImg=zeros(size(bw,1),size(bw,2),3);
+rgbImg=putBwlineOnRgb(rgbImg,bw,[255 255 255]);
+figure,imshow(putBwlineOnRgb(rgbImg,thinRes,[255 0 0]));
 
+addpath(genpath('BaiSkeletonPruningDCE/'));
 tic;
-baiRes=div_skeleton_new(4,1,1-bw,handles.skelVerNum);
+baiRes=div_skeleton_new(4,1,1-bw,13);
 t2=toc;
+baiRes=baiRes~=0;
+
+rgbImg=zeros(size(bw,1),size(bw,2),3);
+rgbImg=putBwlineOnRgb(rgbImg,bw,[255 255 255]);
+figure,imshow(putBwlineOnRgb(rgbImg,baiRes,[255 0 0]));
+
+figure, bar([t1 t2]);
+xlabel('Methods');
+ylabel('Time (s)');
+set(gca,'xticklabel',{'Thinning','Bai''s method'});
 
 end
