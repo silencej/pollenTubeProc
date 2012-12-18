717fb412e2d84270001a9a232acf9c17a1752c5b
Chaofeng Wang <owen263@gmail.com>
2012-12-18 19:24:47 +0800

    Small modi.

--- segPollen/houghEllip.m
+++ segPollen/houghEllip.m
@@ -1,16 +1,10 @@
-function [qlt, scl, rot, xpk, ypk]=houghEllip(src, patt, rhoRange, thetaRange, kwidth)
+function [qlt, scl, rot, xpk, ypk]=houghEllip(eg, xRange, yRange, aRange, bRange, thetaRange)
 %
-% [qlt, scl, rot, varargout] = GFHT(src, patt, rhorange, thetarange, kwidth, varargin)
-% This function implements the generalized fuzzy Hough transform looking 
-% for the best pattern mathing on the source image.
+% This function implements the Hough transform looking for the ellipses.
 %
-% src:      source image (bitmap)
-% patt:     R-table including the points for the searched pattern
-% rhorange: value range for the radius of the pattern
+% eg:      edge image. 2-bit.
 % thetarange: value range for the angle of rotation of the pattern
-% kwidth:   kernel width to be considered for the fuzzy transform. It is
-%           centered on the feature line to get the fuzzy Hough accumulator
-% varargin{1}: debug flag
+%
 % qlt:      quality ratio estimated for the best match
 % scl:      best pattern scale (rho) for the best match
 % rot:      best pattern rotation angle (theta) for the best match
@@ -19,8 +13,7 @@ function [qlt, scl, rot, xpk, ypk]=houghEllip(src, patt, rhoRange, thetaRange, k
 %
 % Copyright (c) 2012 by Chaofeng Wang.
 
-% function [scl, rot, cx, cy]=houghEllipse(eg,rhoRange,thetaRange,)
-% [qlt, scl, rot, xpk, ypk] = gfht(src, patt, rhorange, thetarange, kwidth, varargin)
+
 
 
 end
