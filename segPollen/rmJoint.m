da6d4ca0e50e70a3fd159933a462158990fa31b8
Chaofeng Wang <owen263@gmail.com>
2012-12-24 22:35:38 +0800

    Workable rmJoint.m. Add gaussian smoothing before canny edge application in segPollen.m

--- segPollen/rmJoint.m
+++ segPollen/rmJoint.m
@@ -4,6 +4,50 @@ function pskel=rmJoint(pskel)
 %
 % Copyright: 2012, Chaofeng Wang, PICB, owen263@gmail.com.
 
+global gImg;
 
+gImg=pskel;
+% vis=pskel;
+
+[row col]=find(pskel);
+for i=1:length(row)
+%     vis(row(i),col(i))=0;
+    if ~gImg(row(i),col(i))
+        continue;
+    end
+    [nbrs isNbr4]=nbr8([row(i) col(i)]);
+    if size(nbrs,1)==3
+        if isNbr4(1)
+            gImg(nbrs(1,1),nbrs(1,2))=0;
+        end
+        gImg(row(i),col(i))=0;
+    end
+    if size(nbrs,1)==4
+        gImg(row(i),col(i))=0;
+%         gImg(row-2:row+2,col-2:col+2)
+%         disp('Error: windhole appears!');
+    end
+%     [row col]=find(vis,1);
+% Ren-shape: rm the center pixel plus one 4-nbr.
+%  @
+%  @@
+% @
+% Common joint: just rm the center pixel.
+% @ @
+%  @
+% @
+% Windhole
+% Case 1: it could not exist since each branch having a common joint
+% which will be trimmed.
+%  @
+% @ @
+%  @
+% Case 2:  just remove center pixels.
+% @ @
+%  @
+% @ @
+end
+
+pskel=gImg;
 
 end
--- segPollen/segPollen.m
+++ segPollen/segPollen.m
@@ -61,7 +61,8 @@ img=imread('../../data/guanPollen/G11-1.tif');
 % figure,imshow(img2,[]);
 % figure,imshow(im2bw(img2,graythresh(img2)));
 
-% pg=imfilter(img,fspecial('gaussian',[9,9],1.5),'replicate');
+% Smooth so that the inner edge becomes little.
+img=imfilter(img,fspecial('gaussian',[9,9],1.5),'replicate');
 % % figure,imshow(img2,[]);
 % pg=imopen(pg,strel('ball',50,63000,8));
 % pg=img-pg;
@@ -76,80 +77,85 @@ pgBw=ed1;
 % pgBw=bwmorph(pgBw,'skel',inf);
 figure,imshow(pgBw);
 
-addpath(genpath('sam'));
-[bestEllipses, e] = findellipse(pgBw);
+pgBw=parsiSkel(pgBw);
+pgBw=rmJoint(pgBw);
+figure,imshow(pgBw);
 
-img=imclose(img,strel('disk',3,8));
-figure,imshow(img,[]);
-% figure,imshow(im2bw(img,graythresh(img)));
-
-% img2=imfilter(img,fspecial('gaussian',[3,3],1));
-img=imfill(img,'holes');
-figure,imshow(img,[]);
-bg=imopen(img,strel('ball',50,20000,8));
-img=img-bg;
-figure,imshow(img,[]);
-
-img2=imclose(img,strel('ball',3,10000,8));
-figure,imshow(img2,[]);
-figure,imshow(im2bw(img2,graythresh(img2)));
-
-img=imadjust(img);
-img=adapthisteq(img);
-img(img<=30000)=30000;
-figure,imshow(img,[]);
-ed1=edge(img,'canny',[0.005,0.4],1);
-figure,imshow(ed1);
-figure,imshow(imfill(ed1,'holes'));
-
-% figure;
-% imshow(img,[]);
-% eh=imellipse;
-% pos=wait(eh); % [tlRow tlCol width height], tl: top left.
-% % tempImg=ellipCrop(img,pos);
-% mask=poly2mask(pos(:,1),pos(:,2),size(img,1),size(img,2));
-% % eh=imellipse(gca,[10 10 ]);
-% patt=bwperim(mask);
-% [row col]=find(patt);
-% permOdd=1:2:length(row);
-% permEven=2:2:length(row);
-% permEven=permEven(end:-1:1);
-% perm=[permOdd permEven];
-% row=row(perm);
-% col=col(perm);
-% patt=[col row];
-% save('patt.mat','patt');
-
-load 'patt.mat';
-patt=traceContour(patt);
-figure,plot(patt(:,1),patt(:,2));
-
-addpath(genpath('fght'));
-rhorange = 0.9:0.1:1.2;
-thetarange = -pi/40:pi/60:pi/40;
-kwidth = 5;
-[qlt, scl, rot, xpk, ypk] = gfht(imcomplement(img), patt, rhorange, thetarange, kwidth);
-
-img=imcomplement(img);
-img=imadjust(img);
-% figure,imshow(img);
-% img2=adapthisteq(img,'Distribution','uniform');
-% figure,imshow(img2);
-img=adapthisteq(img,'Distribution','exponential');
-img(img<=haloThre)=0;
-% figure,imshow(img);
-% img=imfilter(img,fspecial('disk', 3));
-% ui(img);
-% rayleigh
-bw=img<2500;
-ed=edge(img,'canny');
-bw=bw&ed;
-showBw;
-bw=imfill(bw,'holes');
-bw=imerode(bw,strel('disk',10));
-fH=figure;
-showBw;
-% imshow(bw1);
+% 
+% addpath(genpath('sam'));
+% [bestEllipses, e] = findellipse(pgBw);
+% 
+% img=imclose(img,strel('disk',3,8));
+% figure,imshow(img,[]);
+% % figure,imshow(im2bw(img,graythresh(img)));
+% 
+% % img2=imfilter(img,fspecial('gaussian',[3,3],1));
+% img=imfill(img,'holes');
+% figure,imshow(img,[]);
+% bg=imopen(img,strel('ball',50,20000,8));
+% img=img-bg;
+% figure,imshow(img,[]);
+% 
+% img2=imclose(img,strel('ball',3,10000,8));
+% figure,imshow(img2,[]);
+% figure,imshow(im2bw(img2,graythresh(img2)));
+% 
+% img=imadjust(img);
+% img=adapthisteq(img);
+% img(img<=30000)=30000;
+% figure,imshow(img,[]);
+% ed1=edge(img,'canny',[0.005,0.4],1);
+% figure,imshow(ed1);
+% figure,imshow(imfill(ed1,'holes'));
+% 
+% % figure;
+% % imshow(img,[]);
+% % eh=imellipse;
+% % pos=wait(eh); % [tlRow tlCol width height], tl: top left.
+% % % tempImg=ellipCrop(img,pos);
+% % mask=poly2mask(pos(:,1),pos(:,2),size(img,1),size(img,2));
+% % % eh=imellipse(gca,[10 10 ]);
+% % patt=bwperim(mask);
+% % [row col]=find(patt);
+% % permOdd=1:2:length(row);
+% % permEven=2:2:length(row);
+% % permEven=permEven(end:-1:1);
+% % perm=[permOdd permEven];
+% % row=row(perm);
+% % col=col(perm);
+% % patt=[col row];
+% % save('patt.mat','patt');
+% 
+% load 'patt.mat';
+% patt=traceContour(patt);
+% figure,plot(patt(:,1),patt(:,2));
+% 
+% addpath(genpath('fght'));
+% rhorange = 0.9:0.1:1.2;
+% thetarange = -pi/40:pi/60:pi/40;
+% kwidth = 5;
+% [qlt, scl, rot, xpk, ypk] = gfht(imcomplement(img), patt, rhorange, thetarange, kwidth);
+% 
+% img=imcomplement(img);
+% img=imadjust(img);
+% % figure,imshow(img);
+% % img2=adapthisteq(img,'Distribution','uniform');
+% % figure,imshow(img2);
+% img=adapthisteq(img,'Distribution','exponential');
+% img(img<=haloThre)=0;
+% % figure,imshow(img);
+% % img=imfilter(img,fspecial('disk', 3));
+% % ui(img);
+% % rayleigh
+% bw=img<2500;
+% ed=edge(img,'canny');
+% bw=bw&ed;
+% showBw;
+% bw=imfill(bw,'holes');
+% bw=imerode(bw,strel('disk',10));
+% fH=figure;
+% showBw;
+% % imshow(bw1);
 
 end
 
